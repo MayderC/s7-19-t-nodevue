1382c222767a45d552d71bd7ee64e7e1ad39ac84
Luis Alomia <luisfidel44@gmail.com>
2023-04-03 16:45:00 -0500

    refactor(stack): stackRepo and repoImpl, cretaeStack useCase and controller

--- backEnd/src/contexts/devSarrolloIt/Stack/domain/repositories/StackRepository.ts
+++ backEnd/src/contexts/devSarrolloIt/Stack/domain/repositories/StackRepository.ts
@@ -3,6 +3,7 @@ import { StackModel } from "../estities/StackEntity";
 interface StackRepository {
   create(stack: StackModel): Promise<StackModel>;
   findAllStack(name?: string): Promise<StackModel[]>;
+  findByNameStack(name: string): Promise<StackModel | null>;
 }
 
 export { type StackRepository };
