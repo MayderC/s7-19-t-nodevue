e0ad18d59d732dc1db782962cbaf982a02cb703d
Manuel <nacermanuel@gmail.com>
2023-04-03 19:42:37 -0500

    feat(role): add endpoint GET/role

--- backEnd/src/app/devSarrolloIt/routers/roleRoutes.ts
+++ backEnd/src/app/devSarrolloIt/routers/roleRoutes.ts
@@ -1,6 +1,7 @@
 import { Request, Response, Router } from "express";
 import { asyncHandler } from "../../shared/framework/asyncHandler";
 import { CreateRoleController } from "../controllers/role/CreateRoleController";
+import { FindAllRoleController } from "../controllers/role/FindAllRoleController";
 
 const roleRouter = Router() ;
 
@@ -10,6 +11,15 @@ roleRouter.post(
         const createRoleController = new CreateRoleController() ;
 
         await createRoleController.run(req, res);
+    })
+)
+
+roleRouter.get(
+    '/role',
+    asyncHandler(async (req: Request, res: Response)=>{
+        const findallrolecontroller = new FindAllRoleController();
+
+        await findallrolecontroller.run(req, res);
     })
 )
 
