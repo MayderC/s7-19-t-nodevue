480e299abf8eea8ef3b08b34549a2534b17a5b94
Manuel <nacermanuel@gmail.com>
2023-04-18 09:00:08 -0500

    feat(publication): Add endpoint Update Publication

--- backEnd/src/contexts/devSarrolloIt/Publication/domain/repositories/PublicationRepository.ts
+++ backEnd/src/contexts/devSarrolloIt/Publication/domain/repositories/PublicationRepository.ts
@@ -6,6 +6,7 @@ interface PublicationRepository {
     findAllPublication: () => Promise<Publication[] | null>
     save:(onePublication: Publication) => Promise<Publication | null>
     getPublicationsByUser:(id: string) => Promise<Publication[] | null>
+    updatePublication:(publicationId:string, update: Publication ) => Promise<Publication> 
 }
 
 export {type PublicationRepository}
