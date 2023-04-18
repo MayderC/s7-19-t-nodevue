37e05402c6156a1e86cebf9dce86df37082fc5cb
Manuel <nacermanuel@gmail.com>
2023-04-18 11:33:06 -0500

    feat(comment): Create Comment model

---
+++ backEnd/src/contexts/devSarrolloIt/Comment/infrastructure/MongooseCommentModel.ts
@@ -0,0 +1,29 @@
+import { Schema, model } from "mongoose";
+import { CommentEntity } from "../domain/entities/CommentEntity";
+
+
+const MongooseCommentSchema = new Schema<CommentEntity>({
+    id:{
+        type: String,
+        required: true,
+    },
+    description:{
+        type: String,
+        required: true,
+    },
+    publicationid:{
+        type: String,
+        required: true,
+    },  
+    userid:{
+        type: String,
+        required: true,
+    },
+});
+
+const MongooseCommentModel = model<CommentEntity>(
+    "Comment",
+    MongooseCommentSchema
+);
+
+export {MongooseCommentModel}
