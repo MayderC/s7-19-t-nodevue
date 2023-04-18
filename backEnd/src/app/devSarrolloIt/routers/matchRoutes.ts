5eec91ea806ddc1512a4a5dee249e4d1d2961387
Matias <matt.estunlp@gmail.com>
2023-04-18 10:41:34 -0300

    feat(match): add endpoint GET /matchsByProject

--- backEnd/src/app/devSarrolloIt/routers/matchRoutes.ts
+++ backEnd/src/app/devSarrolloIt/routers/matchRoutes.ts
@@ -2,6 +2,7 @@ import { Request, Response, Router } from "express";
 import { asyncHandler } from "../../shared/framework/asyncHandler";
 import { CreateMatchController } from "../controllers/match/CreateMatchController";
 import { FindMatchbyUserIdController } from "../controllers/match/FindMatchbyUserIdController";
+import {ShowMatchsByProjectController  } from "../controllers/match/ShowMatchsController";
 
 const matchRouter = Router();
 
@@ -18,6 +19,16 @@ matchRouter.get(
     '/match/:userid',
     asyncHandler(async (req: Request, res: Response)=> {
         const findMatchbyuseridcontroller = new FindMatchbyUserIdController();
+
+        await findMatchbyuseridcontroller.run(req,res)
+
+    })
+)
+
+matchRouter.get(
+    '/matchsByProject',
+    asyncHandler(async (req: Request, res: Response)=> {
+        const findMatchbyuseridcontroller = new ShowMatchsByProjectController();
 
         await findMatchbyuseridcontroller.run(req,res)
 
